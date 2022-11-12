310e2f9cc03d23b4175d19a31788637843180dfa
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-11-12 15:31:31 -0500

    working on bar chart

--- src/app/bar/bar.component.ts
+++ src/app/bar/bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-bar',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarComponent implements OnInit {
 
+  private data = [
+    {"Framework": "Vue", "Stars": "166443", "Released": "2014"},
+    {"Framework": "React", "Stars": "150793", "Released": "2013"},
+    {"Framework": "Angular", "Stars": "62342", "Released": "2016"},
+    {"Framework": "Backbone", "Stars": "27647", "Released": "2010"},
+    {"Framework": "Ember", "Stars": "21471", "Released": "2011"},
+  ];
+
+  // used in the class to store SVG image that D3 draws onto the DOM
+  private svg;
+  private margin = 50;
+  private width = 750 - (this.margin * 2);
+  private height = 400 - (this.margin * 2);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // select element in the DOM and insert a new SVG with a <g> element
+  private createSvg(): void {
+    this.svg = d3.select("figure#bar")
+        .append("svg")
+        .attr("width", this.width + (this.margin * 2))
+        .attr("height", this.height + (this.margin * 2))
+        .append("g")
+        .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
   }
 
 }
