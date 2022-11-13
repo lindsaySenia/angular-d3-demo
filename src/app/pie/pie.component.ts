0da461cb5a6bb6546d5fcac253c81566850993e6
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-11-13 00:54:21 -0500

    pie chart complete

--- src/app/pie/pie.component.ts
+++ src/app/pie/pie.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-pie',
@@ -7,9 +8,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PieComponent implements OnInit {
 
+  private data = [
+    {"Framework": "Vue", "Stars": "166443", "Released": "2014"},
+    {"Framework": "React", "Stars": "150793", "Released": "2013"},
+    {"Framework": "Angular", "Stars": "62342", "Released": "2016"},
+    {"Framework": "Backbone", "Stars": "27647", "Released": "2010"},
+    {"Framework": "Ember", "Stars": "21471", "Released": "2011"},
+  ];
+
+  private svg;
+  private margin = 50;
+  private width = 750;
+  private height = 600;
+
+  //the radius of the pie chart is half the smallest side
+  private radius = Math.min(this.width, this.height) / 2 - this.margin;
+  private colors;
+
+  private createSvg(): void {
+    this.svg = d3.select("figure#pie")
+                  .append("svg")
+                  .attr("width", this.width)
+                  .attr("height", this.height)
+                  .append("g")
+                  .attr(
+                    "transform",
+                    "translate(" + this.width / 2 + "," + this.height / 2 + ")"
+                  );
+  }
+
+  // use an ordinal scale to create a discrete color for each section of the chart
+  private createColors(): void {
+    this.colors = d3.scaleOrdinal()
+                    .domain(this.data.map(d => d.Stars.toString()))
+                    .range(["#c7d3ec", "#a5b8db", "#879cc4", "#677795", "#5a6782"]);
+  }
+
+  // create a method to draw the chart and add labels
+  // use <path> elements to create arcs and fill them with defined colors
+  private drawChart(): void {
+    // Compute the position of each group on the pie:
+    const pie = d3.pie<any>().value((d: any) => Number(d.Stars));
+
+    // Build the pie chart
+    this.svg.selectAll('pieces')
+            .data(pie(this.data))
+            .enter()
+            .append('path')
+            .attr('d', d3.arc()
+                         .innerRadius(0)
+                         .outerRadius(this.radius))
+            .attr('fill', (d, i) => (this.colors(i)))
+            .attr("stroke", "#121926")
+            .style("stroke-width", "1px");
+
+    // Add labels
+    const labelLocation = d3.arc()
+                            .innerRadius(100)
+                            .outerRadius(this.radius);
+
+    this.svg.selectAll('pieces')
+            .data(pie(this.data))
+            .enter()
+            .append('text')
+            .text(d => d.data.Framework)
+            .attr("transform", d => "translate(" + labelLocation.centroid(d) + ")")
+            .style("text-anchor", "middle")
+            .style("font-size", 15);
+
+
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+      this.createSvg();
+      this.createColors();
+      this.drawChart();
   }
 
 }
