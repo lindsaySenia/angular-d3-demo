bcd611ea6e5ae73982210575bc74c34f8935ec47
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2022-11-13 00:32:57 -0500

    bar chart complete

--- src/app/bar/bar.component.ts
+++ src/app/bar/bar.component.ts
@@ -25,6 +25,8 @@ export class BarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.createSvg();
+    this.drawBars(this.data);
   }
 
   // select element in the DOM and insert a new SVG with a <g> element
@@ -35,6 +37,38 @@ export class BarComponent implements OnInit {
         .attr("height", this.height + (this.margin * 2))
         .append("g")
         .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
+  }
+
+  private drawBars(data: any[]): void {
+    // Create the X-axis band scale
+    const x = d3.scaleBand()
+                .range([0, this.width])
+                .domain(data.map(d => d.Framework))
+                .padding(0.2);
+
+    // Draw the X-axis on the DOM
+    this.svg.append("g")
+            .attr("transform", "translate(0," + this.height + ")")
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+            .attr("transform", "translate(-10,0)rotate(-45)")
+            .style("text-anchor", "end");
+        
+    // Create the Y-axis band scale
+    const y = d3.scaleLinear()
+                .domain([0, 200000])
+                .range([this.height, 0]);
+
+    // Draw the Y-axis on the DOM
+    this.svg.selectAll("bars")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", d => x(d.Framework))
+            .attr("y", d => y(d.Stars))
+            .attr("width", x.bandwidth())
+            .attr("height", (d) => this.height - y(d.Stars))
+            .attr("fill", "#d04a35");
   }
 
 }
